341f174830060968807fbe81e43081e679fc8e8c
Rafael Guimaraes <raf.plguimaraes@gmail.com>
2023-07-12 14:57:12 -0300

    Update On NavBar.vue
    Adjusting the app's title

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,33 @@
 <template>
-    <h1><a href="#">SideDoor - Lista de Compras Digital</a></h1>
+    <h1><a href="#">Digital Shopping List</a></h1>
 </template>
 
 <script>
-    export default {
-        name: 'NavBar'
-    }
+export default {
+    name: 'NavBar'
+}
 </script>
 
 <style scoped>
-    h1 {
-        height: 15vh;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 2.5em;
-        font-weight: 300;
-        background-color: #181818;
-        padding: 15px 0;
-    }
+h1 {
+    height: 15vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2.5em;
+    font-weight: 300;
+    background-color: #181818;
+    padding: 15px 0;
+}
 
-    a {
-        color: #fff;
-        text-decoration: none;
-        transition: .3s;
-    }
+a {
+    color: #fff;
+    text-decoration: none;
+    transition: .3s;
+}
 
-    a:hover {
-        color: #FCBA03;
-    }
+a:hover {
+    color: #FCBA03;
+}
 </style>
